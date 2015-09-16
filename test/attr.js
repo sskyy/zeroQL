c641e730008f850c2d96da12356bc7fe134864f2
加缪 <jiamiu.hzy@alibaba-inc.com>
2015-09-16 20:39:14 +0800

    support object in attr

--- test/attr.js
+++ test/attr.js
@@ -3,14 +3,29 @@ var AttrParser = require('../lib/AttrParser')
 
 describe('attrs',function(){
 
-  var parser = new AttrParser
 
-  parser.parse('id,name:"jhon",gender')
   it('ast', function(){
+    var parser = new AttrParser
+
+    parser.parse('id,name:"jhon",gender')
     assert.equal( parser.ast.data.id, null)
     assert.equal( parser.ast.data.name, 'jhon')
     assert.equal( parser.ast.unfilledKeys[0], 'id')
     assert.equal( parser.ast.unfilledKeys[1], 'gender')
+  })
+
+
+  it('array', function(){
+    var parser = new AttrParser
+    parser.parse('id:[1,2,3,4]')
+    assert.equal( parser.ast.data.id.length, 4)
+    assert.equal( parser.ast.data.id.join(','), '1,2,3,4')
+  })
+
+  it('object', function(){
+    var parser = new AttrParser
+    parser.parse(`id:{like:'%something%'}`)
+    assert.equal( parser.ast.data.id.like, '%something%')
   })
 
 })
